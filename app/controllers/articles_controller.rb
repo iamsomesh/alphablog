b4dc048c09d74240e94000941e91d04d36a92da3
Somesh Yadav <syb777yadav@gmail.com>
2022-10-15 04:30:20 +0530

    Create article and added flash & error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,22 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render "new"
+    end
+    # redirect_to article_path(@article) #we can redirect to article/id here @article gives id
+    # render plain: params[:article]
+  end
+
   def show
     @article = Article.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
 end
