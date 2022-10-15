48120659d313f79f264998712cbac026ab5fefae
Somesh Yadav <syb777yadav@gmail.com>
2022-10-16 04:05:17 +0530

    Adding destroy action and adding links to all pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
@@ -34,7 +38,9 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
+  def destroy
     @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 end
